b2e603c4f72f0d7e040dacc4a51c22babea96a50
Vipassna Gautam <gautamvipassna@gmail.com>
2020-06-25 02:42:07 +0530

    Task 15 added

---
+++ Task 15/15.1.js
@@ -0,0 +1,10 @@
+ <script>
+  document.getElementsByTagName("img");
+  document.getElementById("mc_embed_signup");
+  document.getElementsByClassName("stats");
+  document.getElementsByClassName("opener")[0];
+  document.querySelector(".navigation");
+  document.querySelector(".logo img");
+  document.querySelectorAll(".press-logos img");
+  document.querySelectorAll("circle");
+  </script>
